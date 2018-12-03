1922ef2bffc2ad2213d2582e0a37956b6cc75c6e
dimmaryanto93 <software.dimas_m93@icloud.com>
2018-12-03 11:15:08 +0700

    membuat simple entity

--- src/main/java/com/tabeldata/springhibernateexample/SpringHibernateExampleApplication.java
+++ src/main/java/com/tabeldata/springhibernateexample/SpringHibernateExampleApplication.java
@@ -2,11 +2,13 @@ package com.tabeldata.springhibernateexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class SpringHibernateExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringHibernateExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHibernateExampleApplication.class, args);
+    }
 }
---
+++ src/main/java/com/tabeldata/springhibernateexample/entity/Nasabah.java
@@ -0,0 +1,41 @@
+package com.tabeldata.springhibernateexample.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "master_nasabah")
+public class Nasabah {
+
+    @Id
+    @Column(name = "nomor_nasabah", nullable = false, length = 50)
+    private String cif;
+
+    @Column(name = "nama_lengkap", nullable = false, length = 100)
+    private String namaLengkap;
+
+    @Lob
+    @Column(name = "alamat_domisili")
+    private String alamat;
+
+    @Column(name = "tanggal_lahir", nullable = false)
+    private Date tanggalLahir;
+
+    @Column(name = "is_active", nullable = false)
+    private boolean active;
+
+    @Column(name = "saldo_keseluruhan")
+    private BigDecimal saldo;
+
+    @Column(name = "bunga_per_anum")
+    private Double bungaPerAnum;
+
+    @Column(name = "jumlah_transaksi")
+    private Integer jumlahTransaksi;
+
+    @Column(name = "tanggal_permohonan")
+    private Timestamp tanggalPermohonan;
+
+}
