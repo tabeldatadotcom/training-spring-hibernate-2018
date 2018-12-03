bf8803dfa076aa70b8f981eb6b4bbc10790f59a0
dimmaryanto93 <software.dimas_m93@icloud.com>
2018-12-03 10:19:36 +0700

    update project

---
+++ src/main/java/com/tabeldata/springhibernateexample/SpringHibernateExampleApplication.java
@@ -0,0 +1,12 @@
+package com.tabeldata.springhibernateexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernateExampleApplication.class, args);
+	}
+}
