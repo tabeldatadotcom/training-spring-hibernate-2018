dc81aaf28c2cf858b5e6063639829bf18e913b75
dimmaryanto93 <software.dimas_m93@icloud.com>
2018-12-03 14:36:35 +0700

    save relation many to one

--- src/main/java/com/tabeldata/springhibernateexample/SpringHibernateExampleApplication.java
+++ src/main/java/com/tabeldata/springhibernateexample/SpringHibernateExampleApplication.java
@@ -1,6 +1,8 @@
 package com.tabeldata.springhibernateexample;
 
+import com.tabeldata.springhibernateexample.entity.Deposito;
 import com.tabeldata.springhibernateexample.entity.Nasabah;
+import com.tabeldata.springhibernateexample.repository.DepositoDao;
 import com.tabeldata.springhibernateexample.repository.NasabahDao;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -23,40 +25,33 @@ public class SpringHibernateExampleApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringHibernateExampleApplication.class, args);
 
-//        NasabahDao bean = applicationContext.getBean(NasabahDao.class);
-//        bean.save(new Nasabah(
-//                "001",
-//                "Dimas Maryanto",
-//                "bukit indah no b8",
-//                Date.valueOf(LocalDate.now()),
-//                false,
-//                new BigDecimal(0),
-//                0d,
-//                1,
-//                Timestamp.valueOf(LocalDateTime.now()))
-//        );
-//
-//        bean.save(new Nasabah(
-//                "002",
-//                "Dimas Maryanto",
-//                "bukit indah no b8",
-//                Date.valueOf(LocalDate.now()),
-//                false,
-//                new BigDecimal(0),
-//                0d,
-//                1,
-//                Timestamp.valueOf(LocalDateTime.now()))
-//        );
-//
-//        Optional<Nasabah> nasabahBaru = bean.findById("001");
-//        Nasabah n001 = nasabahBaru.get();
-//        log.info("nasabah id = 001 : {}", n001);
-//
-//        Iterable<Nasabah> list = bean.findAll();
-//        for (Nasabah nasabah : list) {
-//            log.info("{}", nasabah);
-//        }
-//
-//        bean.deleteById("002");
+        NasabahDao nasabahDao = applicationContext.getBean(NasabahDao.class);
+        DepositoDao depositoDao = applicationContext.getBean(DepositoDao.class);
+
+        Nasabah n001 = nasabahDao.save(
+                new Nasabah(
+                        "001",
+                        "Dimas Maryanto",
+                        "bukit indah no b8",
+                        Date.valueOf(LocalDate.now()),
+                        false,
+                        new BigDecimal(0),
+                        0d,
+                        1,
+                        Timestamp.valueOf(LocalDateTime.now())
+                ));
+
+        Deposito deposito = new Deposito(
+                null,
+                n001,
+                3,
+                BigDecimal.ZERO,
+                BigDecimal.ZERO,
+                BigDecimal.ZERO);
+        Deposito d1 = depositoDao.save(deposito);
+
+        Optional<Deposito> dep1 = depositoDao.findById(d1.getId());
+        log.info("deposito new --> : {}", dep1.get());
+
     }
 }
